923f2bb12500300ea428497083650578ca968892
Youssef Bouaouda <ybouaoud@c4r10p2.1337.ma>
2023-12-29 05:09:49 +0100

    Updated script

--- monitoring.sh
+++ monitoring.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 arc=$(uname -a)
-lscpu=$(lscpu | awk '/^Socket\(s\):/ { print $2 }')
-vcpu=$(nproc)
+lscpu=$(lscpu | grep 'Socket(s):' | awk '{ print $2 }')
+vcpu=$(lscpu | awk 'NR==5 {print $2}')
 tram=$(free -m | awk 'NR==2 { print $2}')
 uram=$(free -m | awk 'NR==2 { print $3}')
 lcpu=$(uptime | awk '{print $9}' | tr -d ',')
-disk_infos=$(df -h / | awk 'NR==2 {print $3, $2, $5}')
-read udik adik upm <<< "$disk_infos"
-pram=$(awk "BEGIN {printf \"%.2f\", $uram / $tram * 100}")
+disk_T=$(df --total -h | grep 'total' | awk '{print $2}')
+udisk=$(df --total -h | grep 'total' | awk '{print $3}')
+upm=$(df --total -h | grep 'total' | awk '{printf "%.2f", $3/$2 * 100}')
+pram=$(free -m | awk 'NR==2 {printf "%.2f", $3/$2 * 100}')
 lbt=$(uptime -s)
-if [ -d "/dev/mapper" ]; then
+if [ "lsblk | grep 'LVM' | wc -l" ]; then
 	lvma="yes"
 else
 	lvma="no"
 fi
 acon=$(ss -t -a state established | wc -l)
-unum=$(who | awk '{print $1}' | sort -u | wc -l)
-ipv4=$(ip -o -4 addr show | grep inet | awk 'NR==2 {print $4}')
+unum=$(who --count | grep 'users' | tr '=' ' ' | awk '{print $3}')
+ipv4=$(hostname -I)
 MAC=$(ip link show | grep ether | awk '{print $2}')
 scom=$(journalctl -q _COMM=sudo | grep COMMAND | wc -l)
 wall 	"
@@ -24,13 +25,12 @@ wall 	"
 	#CPU physical    : $lscpu
 	#vCPU	         : $vcpu
 	#Memory Usage    : $uram"M"/$tram"G" ($pram%)
-	#Disk Usage      : $udik/$adik ($upm)
+	#Disk Usage      : $udisk/$disk_T ($upm%)
 	#CPU load        : $lcpu%
 	#Last boot       : $lbt
 	#LVM use         : $lvma
-	#Connections TCP : $acon
+	#Connections TCP : $acon ESTABLISHED
 	#User log	 : $unum
-	#Network	 : IP $ipv4 ($MAC)
+	#Network	 : IP $ipv4($MAC)
 	#Sudo		 : $scom cmd
 	"
-
